3e94bc5040efc47eb1f86a781779afc4e5cfe360
Misiaczek <Mosormichal711@gmail.com>
2023-03-29 18:48:51 +0200

    PA-03:circle ready

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -4,7 +4,22 @@ import './Circle.css'; // Import the CSS file with styles
 function Circle() {
   return (
     <div className='root'>
-        <div className="pie">
+        <div className="circle">
+          <div className="slice">
+            <span>Michał</span>
+          </div>
+          <div className="slice">
+          <span>Kuba</span>
+          </div>
+          <div className="slice">
+          <span>Paweł</span>
+          </div>
+          <div className="slice">
+          <span>O&nbsp;Nas</span>
+          </div>
+        </div>
+        
+        {/* <div className="pie">
         <button className='firstPie'>
             <span className="text">Paweł</span>    
         </button>
@@ -14,7 +29,7 @@ function Circle() {
         <button className='thirdPie'>
             <span className="text">Jakub</span>    
         </button>
-        </div>
+        </div> */}
     </div>
   );
 }
