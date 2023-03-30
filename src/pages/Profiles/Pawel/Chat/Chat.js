ff0695445e5940c331937a97f47a2f1c36e1fb09
Pablo <pawel.polakiewicz1@gmail.com>
2023-03-30 21:00:59 +0200

    PA-04: Chat page implemented

--- src/pages/Profiles/Pawel/Chat/Chat.js
+++ src/pages/Profiles/Pawel/Chat/Chat.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import classes from './Chat.module.css'
+import ConsoleHeader from '../../../../components/Pawel/Header/ConsoleHeader';
 
 const PChat = () => {
   return (
-    <div>Chat</div>
+    <div className={classes.root}>
+      <ConsoleHeader text="Chat"/>
+      <div className={classes.chatContainer}>
+        <div className={classes.chatBox}></div>
+      </div>
+    </div>
   )
 }
 
